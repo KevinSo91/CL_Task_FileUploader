4b474376736dc2175899f98b1cc4d1ab6ca82e4e
Kevin S <Kevin.Sommer91@freenet.de>
2022-12-14 17:36:50 +0100

    working on design

---
+++ src/main/java/de/mainPackage/controller/InfoController.java
@@ -0,0 +1,16 @@
+package de.mainPackage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/info")
+public class InfoController {
+	
+	@GetMapping("")
+	public String getInfo() {
+		return "info";
+	}
+	
+}
